b3b949fb7304de094d014d166976cac7544efdff
Komposten <j.hjelm586@hotmail.com>
2019-09-26 18:35:01 +0200

    :construction: Chrome testing - run tests in Chrome with profiler

--- lib/src/chrome_tester.dart
+++ lib/src/chrome_tester.dart
@@ -20,7 +20,10 @@ class ChromeTester extends Tester {
 
   @override
   Future<File> runTestsAndCollect(File entrypoint, bool printTestOutput) async {
-    var htmlFile = await _transpileToJS(entrypoint);
+    stdout.writeln('>> Running dart2js...');
+    var jsFile = await _transpileToJS(entrypoint);
+    var htmlFile = await _copyHtmlFile();
+    await _runTests(htmlFile, jsFile, printTestOutput);
     return File(reportsDir);
   }
 
@@ -42,13 +45,15 @@ class ChromeTester extends Tester {
         .transform(utf8.decoder)
         .transform(const LineSplitter())
         .listen((line) {
-      stdout.writeln('>> $line');
+      if (line != null && !line.contains(RegExp(r'Hint:|Dart file|<sdk>.+preambles'))) {
+        stdout.writeln('>>>> $line');
+      }
     });
     process.stderr
         .transform(utf8.decoder)
         .transform(const LineSplitter())
         .listen((line) {
-      stderr.writeln('>> $line');
+      stderr.writeln('>>>> $line');
     });
 
     var exitCode = await process.exitCode;
@@ -56,7 +61,7 @@ class ChromeTester extends Tester {
       throw 'dart2js failed with exit code $exitCode';
     }
 
-    return await _copyHtmlFile();
+    return outputFile;
   }
 
   Future<File> _copyHtmlFile() async {
@@ -64,5 +69,100 @@ class ChromeTester extends Tester {
     var source = Resource('package:covered/res/chrome-template.html');
 
     return target.writeAsString(await source.readAsString());
+  }
+
+  Future<void> _runTests(
+      File htmlFile, File jsFile, bool printTestOutput) async {
+    var htmlPath = htmlFile.absolute.path;
+    var jsPath = jsFile.absolute.path;
+    var nodeEntrypoint = await _copyNodeEntrypoint();
+
+    var nodeArgs = [
+      nodeEntrypoint.path,
+      '8787',
+      path.context.toUri(htmlPath).toString(),
+      path.context.toUri(jsPath).toString(),
+      printTestOutput.toString()
+    ];
+    stdout.writeln('>> Running npm...');
+    await runNpm(nodeEntrypoint);
+    stdout.writeln('>> Launching Chrome...');
+    var chrome = await _launchChrome();
+    stdout.writeln('>> Running tests...');
+    var process = await Process.start('node', nodeArgs,
+        workingDirectory: nodeEntrypoint.parent.path);
+    process.stdout
+        .transform(utf8.decoder)
+        .transform(const LineSplitter())
+        .listen((line) {
+      stdout.writeln('>>>> $line');
+    });
+    process.stderr
+        .transform(utf8.decoder)
+        .transform(const LineSplitter())
+        .listen((line) {
+      stderr.writeln('>>>> $line');
+    });
+
+    var exitCode = await process.exitCode;
+    chrome.kill(ProcessSignal.sigkill);
+    if (exitCode != 0) {
+      throw 'node failed with exit code $exitCode';
+    }
+
+    return await _copyHtmlFile();
+  }
+
+  Future<void> runNpm(File nodeEntrypoint) async {
+    var npm = await Process.start('npm.cmd', ['install'],
+        workingDirectory: nodeEntrypoint.parent.path);
+    npm.stdout
+        .transform(utf8.decoder)
+        .transform(const LineSplitter())
+        .listen((line) => stdout.writeln('>>>> $line'));
+    npm.stderr
+        .transform(utf8.decoder)
+        .transform(const LineSplitter())
+        .listen((line) => stderr.writeln('>>>> $line'));
+    var exitCode = await npm.exitCode;
+    if (exitCode != 0) {
+      throw 'npm install failed with exit code $exitCode';
+    }
+  }
+
+  Future<File> _copyNodeEntrypoint() async {
+    Directory directory =
+        Directory(path.join(Directory.systemTemp.path, 'covered'));
+    var entryTarget =
+        File(path.join(Directory.systemTemp.path, 'covered', 'chrome.js'));
+    var packageTarget =
+        File(path.join(Directory.systemTemp.path, 'covered', 'package.json'));
+    var entrySource = Resource('package:covered/js/chrome.js');
+    var packageSource = Resource('package:covered/js/package.json');
+
+    await directory.create(recursive: true);
+    await packageTarget.writeAsString(await packageSource.readAsString());
+    await entryTarget.writeAsString(await entrySource.readAsString());
+
+    return entryTarget;
+  }
+
+  Future<Process> _launchChrome() async {
+    var chromeArgs = [
+      '--remote-debugging-port=8787',
+      '--disable-extensions',
+      '--disable-popup-blocking',
+      '--bwsi',
+      '--no-first-run',
+      '--no-default-browser-check',
+      '--disable-default-apps',
+      '--disable-translate'
+      //TODO(komposten): Add a command line option for the port
+    ];
+
+    //FIXME(komposten): Temporary hardcoded chrome path.
+    return await Process.start(
+        r'C:\Program Files (x86)\Google\Chrome\Application\chrome', chromeArgs,
+        workingDirectory: projectDir);
   }
 }
