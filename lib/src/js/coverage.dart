242aebdbcc4c0f9c64b27bb84cd8f29c5de22654
Komposten <j.hjelm586@hotmail.com>
2019-10-16 19:21:36 +0200

    :page_facing_up: Add license to a bunch of files

--- lib/src/js/coverage.dart
+++ lib/src/js/coverage.dart
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2019 Jakob Hjelm (Komposten)
+ *
+ * This file is part of covered.
+ *
+ * covered is a free Dart library: you can use, redistribute it and/or modify
+ * it under the terms of the MIT license as written in the LICENSE file in the root
+ * of this project.
+ */
 import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
--- lib/src/lcov.dart
+++ lib/src/lcov.dart
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2019 Jakob Hjelm (Komposten)
+ *
+ * This file is part of covered.
+ *
+ * covered is a free Dart library: you can use, redistribute it and/or modify
+ * it under the terms of the MIT license as written in the LICENSE file in the root
+ * of this project.
+ */
 import 'package:lcov/lcov.dart';
 
 class Lcov {
