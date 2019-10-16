285e428eb176183eab02b381456fd05928f90127
Komposten <j.hjelm586@hotmail.com>
2019-10-16 20:47:27 +0200

    :green_heart: Fix GH Actions build

--- test/utilities_test.dart
+++ test/utilities_test.dart
@@ -7,12 +7,21 @@ void main() {
     expect(testOnPlatform('browser && !chrome', 'chrome'), false);
     expect(testOnPlatform('vm', 'vm'), true);
     expect(testOnPlatform('vm', 'firefox'), false);
-    expect(testOnPlatform('vm && windows', 'vm'), true);
-    expect(testOnPlatform('vm && linux', 'vm'), false);
-    expect(testOnPlatform('vm && !posix', 'vm'), true);
     expect(testOnPlatform('js', 'firefox'), true);
     expect(testOnPlatform('js', 'node'), true);
     expect(testOnPlatform('dart-vm', 'vm'), true);
     expect(testOnPlatform('dart-vm', 'node'), false);
   });
+
+  test('testOnPlatform_onWindows', () {
+    expect(testOnPlatform('vm && windows', 'vm'), true);
+    expect(testOnPlatform('vm && linux', 'vm'), false);
+    expect(testOnPlatform('vm && !posix', 'vm'), true);
+  }, testOn: 'windows');
+
+  test('testOnPlatform_onLinux', () {
+    expect(testOnPlatform('vm && windows', 'vm'), false);
+    expect(testOnPlatform('vm && linux', 'vm'), true);
+    expect(testOnPlatform('vm && !posix', 'vm'), false);
+  }, testOn: 'linux');
 }
